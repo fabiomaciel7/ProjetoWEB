78a968fc6e850b25ccf4f91809945ccb605d4af4
fabiomaciel7 <fabio.maciel@ccc.ufcg.edu.br>
2024-09-04 09:45:06 -0300

    [REFACTOR] unnecessary import removed

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response} from 'express';
 import { AuthService } from '../services/AuthService';
 
 export class AuthController {
